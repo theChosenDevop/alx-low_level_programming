f82edf97fb3ea344c0c0f5ab5604dd19d35c07f7
theChosenDevop <tobbey.adesanya@gmail.com>
2023-06-16 07:26:18 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - Entry Point
+* Description: A C program that prints a string
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle")
+	return(0)
+}
