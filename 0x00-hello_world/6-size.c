cf8bd10571c4813f1387ecd5788cc95bdeaa4554
theChosenDevop <tobbey.adesanya@gmail.com>
2023-06-16 08:02:05 -0700

    sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: FInding the size of different variable types
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(a));
+	printf("Size of an int: %lu byte(s)\n", sizeof(b));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(c));
+	printf("Size of long long int %lu byte(s)\n", sizeof(d));
+	printf("Size of a float: %lu byte(s)\n", sizeof(f));
+
+	return (0);
+}
