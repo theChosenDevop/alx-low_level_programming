6f94da3e2c320c4ad5d8dd8a72f9e405dc2616ac
theChosenDevop <tobbey.adesanya@gmail.com>
2023-06-27 10:30:31 -0700

    updateDescription

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98: deference pointer to 98
+ * reset_to_98 - deference pointer to 98
  * @n: input
  * Description: change the address value to 98
  * Return: void
