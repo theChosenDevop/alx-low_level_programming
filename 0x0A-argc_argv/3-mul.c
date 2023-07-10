19374fe39900a817aff5d6780ec7d295a089aab3
theChosenDevop <tobbey.adesanya@gmail.com>
2023-07-10 05:36:09 -0700

    multiply argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return:0 for sucess and 1 for failure
+ */
+
+int main(int argc, char **argv)
+{
+	int multi;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		multi = 1;
+		while (*(++argv))
+		{
+			multi *= _atoi(*argv);
+		}
+	}
+	printf("%d\n", multi);
+	return (0);
+}
