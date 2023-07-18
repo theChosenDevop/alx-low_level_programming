73a9aeb9ada0e874b4f9170f813aba6190ab3b13
theChosenDevop <tobbey.adesanya@gmail.com>
2023-07-17 22:13:38 -0700

    corrected struct files

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,7 +8,6 @@
  * @owner: pointer variable
  * Return: void
  */
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d->name = name;
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,8 +14,9 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-}
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
