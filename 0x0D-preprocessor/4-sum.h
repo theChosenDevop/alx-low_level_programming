2e301eb45cd95f4d093aed03fa07168e6a730469
theChosenDevop <tobbey.adesanya@gmail.com>
2023-07-17 20:35:21 -0700

    sum MAcro

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * File: 4-sum.h
+ * Desc:SUM(x, y) computes the sum of the numbers x and y
+ * @x: input
+ * @y: input
+ */
+
+#define SUM(x, y) (x + y)
+
+#endif
