b5cc8a06767611f613e55b1457ecd77060333a84
theChosenDevop <tobbey.adesanya@gmail.com>
2023-07-17 20:27:36 -0700

    ABS() Macro

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_LIKE_MACRO
+#define FUNCTION_LIKE_MACRO
+
+/**
+ * File: 3-function_like_macro.h
+ * Desc: Function like macro ABS(x)
+ * @x: input
+ */
+
+#define ABS(x) ((x) > 0 ? (-1 * (x)) : (x))
+
+#endif
