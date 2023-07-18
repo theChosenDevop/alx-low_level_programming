5b28b9cc49b74d983cc4c0bd4d359d122487b52f
theChosenDevop <tobbey.adesanya@gmail.com>
2023-07-17 21:14:51 -0700

    struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Dog
+ * Desc: New struct dog
+ * @name: pointer variable
+ * @age: pointer variable
+ * @owner: pointer variable
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
