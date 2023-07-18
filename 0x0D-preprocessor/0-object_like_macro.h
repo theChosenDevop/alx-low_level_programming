6211387a89914c483d9794df859c3d13b77651c1
theChosenDevop <tobbey.adesanya@gmail.com>
2023-07-17 19:47:13 -0700

    PIMacro

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,10 @@
 #ifndef 0BJECT_LIKE_MACRO
 #define 0BJECT_LIKE_MACRO
 
+/**
+ * File: 0-objec_like_macro.h
+ * Desc: assign value of 1024 to macro SIZE
+ */
 
 #define SIZE 1024
 
---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,11 @@
+#ifndef PI_H
+#define PI_H
+
+/**
+ * File: 1-pi.h
+ * Desc: header file defines macro name PI to 3.14159265359
+ */
+
+#define PI 3.1459265359
+
+#endif
