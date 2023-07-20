9720eb354d37f3ac5569e4a9805cac1114517723
theChosenDevop <tobbey.adesanya@gmail.com>
2023-07-20 06:52:57 -0700

    variadic sum function

--- 0-sum_them_all.c/0-sum_them_all.c
+++ 0-sum_them_all.c/0-sum_them_all.c
@@ -1,5 +1,4 @@
 #include <stdarg.h>
-#include "variadic_functions.h"
 
 /**
  * sum_them_all - sum of all its parameter
@@ -15,10 +14,12 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(po, n);
 	sum = 0;
 
+	if (n == 0)
+		return (0);
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(po, int);
 	}
-
+	va_end(po);
 	return (sum);
 }
