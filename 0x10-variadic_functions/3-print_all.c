efad8c3801c04c40855b171dfd558593c828e8e0
theChosenDevop <tobbey.adesanya@gmail.com>
2023-07-27 18:55:07 -0700

    update

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,62 +1,66 @@
-#include <stdarg.h>
-#include <string.h>
 #include <stdio.h>
+#include <stdarg.h>
 #include "variadic_functions.h"
-
 /**
- * print_all - prints anything
- * @format: List of types of arguments passed to the function
- * Return: void
+ * print_all - print variable types
+ * @format: input
  */
-
-void print_all(const char * const format, ...)
+void print_all(const char *const format, ...)
 {
-	va_list ap;
-	char c;
-	int i;
-	double f;
-	char *s;
-	int j;
+    va_list args;
+    unsigned int i = 0, start = 0;
+    char *s;
 
-	va_start(ap, format);
-	j = 0;
-
-	while (format[j])
-	{
-		if (format[j] == 'c')
-		{
-			c = va_arg(ap, int);
-			printf("%c", c);
-		}
-		else if (format[j] == 'i')
-		{
-			i = va_arg(ap, int);
-			printf("%d", i);
-		}
-		else if (format[j] == 'f')
-		{
-			f = va_arg(ap, double);
-			printf("%f", f);
-		}
-		else if (format[j] == 's')
-		{
-			s = va_arg(ap, char *);
-			if (s == NULL)
-			{
-				printf("(nil)");
-			}
-			else
-			{
-				printf("%s", s);
-			}
-		}
-		if (format[j])
-		{
-			printf(", ");
-		}
-		j++;
-	}
-	va_end(ap);
-	putchar('\n');
+    va_start(args, format);
+    while (format[i] != '\0')
+    {
+        switch (format[i])
+        {
+        case 'c':
+            switch (start)
+            {
+            case 1:
+                printf(", ");
+            }
+            start = 1;
+            printf("%c", va_arg(args, int));
+            break;
+        case 'i':
+            switch (start)
+            {
+            case 1:
+                printf(", ");
+            }
+            start = 1;
+            printf("%i", va_arg(args, int));
+            break;
+        case 'f':
+            switch (start)
+            {
+            case 1:
+                printf(", ");
+            }
+            start = 1;
+            printf("%f", va_arg(args, double));
+            break;
+        case 's':
+            switch (start)
+            {
+            case 1:
+                printf(", ");
+            }
+            start = 1;
+            s = va_arg(args, char *);
+            if (s)
+            {
+                printf("%s", s);
+                break;
+            }
+            printf("%p", s);
+            break;
+        }
+        i++;
+    }
+    printf("\n");
+    va_end(args);
 }
-		
