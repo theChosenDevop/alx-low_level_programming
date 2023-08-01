55f5804a9249fdeb77760d85a974efa4687fa6c6
theChosenDevop <tobbey.adesanya@gmail.com>
2023-08-01 15:39:54 -0700

    free list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free listint_t list
+ * @head: input
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *currNode, *temp;
+
+	if (head != NULL)
+	{
+		currNode = *head;
+		while ((temp = currNode) != NULL)
+		{
+			currNode = currNode->next;
+			free(temp);
+		}
+	}
+	*head = NULL;
+}
