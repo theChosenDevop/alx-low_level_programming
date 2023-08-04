8914ddb65e869ff70e4b0afb922b5a7e2c8343e9
theChosenDevop <tobbey.adesanya@gmail.com>
2023-08-03 20:39:00 -0700

    get bits at index

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_bit - sets the value of a bit to 1 at a given index
+ * @n: input
+ * @index: index of bit
+ * Returns: 1 (success) or -1 (failure)
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+
