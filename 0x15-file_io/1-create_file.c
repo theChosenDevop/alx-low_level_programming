5266b0aa33de22dd353b198103e6a879e1fee97c
theChosenDevop <tobbey.adesanya@gmail.com>
2023-08-07 14:09:55 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,12 +24,15 @@ int create_file(const char *filename, char *text_content)
 		close(fd);
 		return (-1);
 	}
-	len = strlen(text_content);
-	wBytes = write(fd, text_content, len);
-	if (wBytes == -1)
+	if (text_content != NULL)
 	{
-		close(fd);
-		return (-1);
+		len = strlen(text_content);
+		wBytes = write(fd, text_content, len);
+		if (wBytes == -1 || wBytes != len)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
 
 	close(fd);
