4bda06897ab42eeb356d054b3b068f65c9f33cbe
theChosenDevop <tobbey.adesanya@gmail.com>
2023-09-08 13:03:53 -0700

    update

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,4 +1,22 @@
-#include "elfheader.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <elf.h>
+
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi_version(unsigned char *e_ident);
+void print_os_abi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry_point_address(unsigned long int e_entry,
+		unsigned char *e_ident);
+void close_elf(int elf);
 
 /**
  * main - entry point
@@ -267,5 +285,47 @@ void print_type(unsigned int e_type, unsigned char *e_ident)
 			break;
 		default:
 			printf("<unknown: %x>\n", e_type);
+	}
+}
+
+/**
+ * print_entry_point_address - Prints the entry point of an Elf header
+ * @e_entry: The address of the Elf entry point
+ * @e_indent: A pointer to an array of Elf header
+ * Return: void
+ */
+void print_entry_point_address(unsigned long int e_entry,
+		unsigned char *e_ident)
+{
+	printf("  Entry point address:               ");
+
+	if (e_ident[EI_DATA] == ELFDATA2MSB)
+	{
+		e_entry = ((e_entry << 8)
+				& 0xFF00FF00) | ((e_entry >> 8) & 0xFF00FF);
+		e_entry = (e_entry << 16) | (e_entry >> 16);
+	}
+
+	if (e_ident[EI_CLASS] == ELFCLASS32)
+	{
+		printf("%#x\n", (unsigned int)e_entry);
+	}
+	else
+	{
+		printf("%#lx\n", e_entry);
+	}
+}
+
+/**
+ * close_elf - Close an Elf file
+ * @elf: The file descriptor of the Elf file
+ * Return: void
+ */
+void close_elf(int elf)
+{
+	if (close(elf) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", elf);
+		exit(98);
 	}
 }
