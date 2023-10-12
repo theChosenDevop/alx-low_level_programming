2a6ba0095c26540479187daf50ff6f7ad38e2371
theChosenDevop <tobbey.adesanya@gmail.com>
2023-10-12 11:36:15 -0700

    defined length function for list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - output length of linked list
+ * @h: head of list
+ * Return: length
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
