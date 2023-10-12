bf4a87c39150e8009a2c957f90b7e1223cf53d6d
theChosenDevop <tobbey.adesanya@gmail.com>
2023-10-12 11:05:56 -0700

    defined printlist function

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of dlistint_t list
+ * @h: head of linked list
+ * Return: elements
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	if (h == NULL)
+		return (1);
+	while (h)
+	{
+		count++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+
+	return (count);
+}
