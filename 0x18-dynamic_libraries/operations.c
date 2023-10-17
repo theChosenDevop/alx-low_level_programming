80f670e6343b52b8fd4100d5d75c69a9a362aeeb
theChosenDevop <tobbey.adesanya@gmail.com>
2023-10-16 17:23:25 -0700

    CPython operations

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,57 @@
+#include "operations.h"
+#include <stdio.h>
+
+/**
+ * add - adds two arguments
+ * @a: input
+ * @b: input
+ * Return: output
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtract two arguments
+ * @a: input
+ * @b: input
+ * Return: output
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mod - modulo of  two arguments
+ * @a: input
+ * @b: input
+ * Return: output
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
+/**
+ * div - div two arguments
+ * @a: input
+ * @b: input
+ * Return: output
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mul - multiply two arguments
+ * @a: input
+ * @b: input
+ * Return: output
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
