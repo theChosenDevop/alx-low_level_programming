ef5ea3448688dce2760c922d3a684723750a471c
theChosenDevop <tobbey.adesanya@gmail.com>
2023-11-04 07:57:49 -0700

    Execute Hashfunction to get key index on hashtable

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - gives the index of a key
+ * @key: input
+ * @size: size of the array of the hash table
+ * Return: index at which key/value ipair should be stored
+ * in the array of the hash table
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	if (!key)
+		return (1);
+	return (hash_djb2(key) % size);
+}
