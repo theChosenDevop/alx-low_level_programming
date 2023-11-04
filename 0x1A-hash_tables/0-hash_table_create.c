7a29b585c8087fc502be07eacad50e5df9eb364d
theChosenDevop <tobbey.adesanya@gmail.com>
2023-11-04 05:26:25 -0700

    hash_table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,16 +1,23 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - creates an hashtable containing
+ * indices of array of key : value pair
+ * @size: size of hashtable
+ * Return: table
+ */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
 	hash_table_t *table;
 
-	table = (hash_table_t*)malloc(sizeof(hash_table_t));
+	table = (hash_table_t *)malloc(sizeof(hash_table_t));
 	table->size = size;
-	table->array = (hash_node_t**)calloc(table->size, sizeof(hash_node_t*));
+	table->array = (hash_node_t **)calloc(table->size, sizeof(hash_node_t *));
 
 	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
 
-	return table;
+	return (table);
 }
