b9a15317723b1b8017ee274d68c8a921b85ba4c5
theChosenDevop <tobbey.adesanya@gmail.com>
2023-11-04 05:41:18 -0700

    hash_table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,7 +4,7 @@
  * hash_table_create - creates an hashtable containing
  * indices of array of key : value pair
  * @size: size of hashtable
- * Return: table
+ * Return: NULL for errors or return a pointer to a new hash table
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
@@ -14,11 +14,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	table = (hash_table_t *)malloc(sizeof(hash_table_t));
 	if (!table)
-		return NULL;
+		return (NULL);
 	table->size = size;
-	table->array = malloc(sizeof(hash_node_t *) * size);
+	table->array = (hash_node_t **)calloc(table->size, sizeof(hash_node_t *));
 	if (!table->array)
-		return NULL;
+		return (NULL);
 
 	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
