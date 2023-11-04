9914d701396b9148be90297c257f4027c8c67270
theChosenDevop <tobbey.adesanya@gmail.com>
2023-11-04 05:36:47 -0700

    hash_table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,8 +13,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table;
 
 	table = (hash_table_t *)malloc(sizeof(hash_table_t));
+	if (!table)
+		return NULL;
 	table->size = size;
-	table->array = (hash_node_t **)calloc(table->size, sizeof(hash_node_t *));
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (!table->array)
+		return NULL;
 
 	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
