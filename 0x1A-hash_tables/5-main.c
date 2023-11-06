0dbea8f753f770896e021ac36a51671b72a1fa90
theChosenDevop <tobbey.adesanya@gmail.com>
2023-11-06 02:30:48 -0800

    delete and print hash table

--- 0x1A-hash_tables/5-main.c
+++ 0x1A-hash_tables/5-main.c
@@ -6,17 +6,17 @@
 /**
  * main - check the code
  *
- * Return: ALways EXIT_SUCCESS.
+ * Return: Always EXIT_SUCCESS.
  */
 int main(void)
 {
 	hash_table_t *ht;
 
-	ht = hash_table_create(1024);
+    ht = hash_table_create(1024);
 	hash_table_print(ht);
 	hash_table_set(ht, "c", "fun");
 	hash_table_set(ht, "python", "awesome");
-	hash_table_set(ht, "Bob", "and kris love asm");
+	hash_table_set(ht, "Bob", "and Kris love asm");
 	hash_table_set(ht, "N", "queens");
 	hash_table_set(ht, "Asterix", "Obelix");
 	hash_table_set(ht, "Betty", "Cool");
