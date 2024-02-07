9477aecdeefa009d979607888bfda6eb2767d5a9
theChosenDevop <tobbey.adesanya@gmail.com>
2024-02-07 15:19:26 -0800

    update

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,7 +14,7 @@ int linear_search(int *array, size_t size, int value)
 	int i = 0;
 	int top = size;
 
-	if (*array == 0)
+	if (array == NULL)
 		return (-1);
 
 	while (i < top)
---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,11 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - Function that searches for a value ina sorted array
+ * of integers using Binary Search Algorithm
+ * @array: A pointer to the first element of the array to search in
+ * @value: The value to search for
+ * @size: The number of elements in array
+ * Return: -1 if value not present in array or rray is NULL or found index
+ */
+
